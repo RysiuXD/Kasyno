60cfda1f5d31f0d9360718ae79a5e20ec9b29848
RysiuXD <dawid.zelinka@gmail.com>
2022-05-26 20:56:25 +0200

    Losowanie i wyświetlanie wylosowanych "slotów?"

---
+++ Kasyno/Classes/SlotMachine.cs
@@ -0,0 +1,34 @@
+﻿namespace Kasyno.Classes
+{
+    public class SlotMachine
+    {
+        public int PunktyGracz, Zetony;
+        public string Komunikat;
+        public bool GameOver;
+        string[] DoWylosowania = { "🍒", "🍇", "💰", "🔔" };
+        public string[] Wylosowane = new string[3];
+
+        public SlotMachine()
+        {
+            Komunikat = "Mozesz uruchomic maszyne";
+            GameOver = false;
+            Zetony = 200;
+
+            
+
+            
+
+           
+        }
+
+        public void Losuj()
+        {
+            for (int i = 0; i < Wylosowane.Length; i++)
+            {
+                var rnd = new Random();
+                Wylosowane[i] = DoWylosowania[rnd.Next(0, DoWylosowania.Length)];
+            }
+        }
+
+    }
+}
---
+++ Kasyno/Controllers/SlotMachineController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Kasyno.Classes;
+
+
+namespace Kasyno.Controllers
+{
+
+public class SlotMachineController : Controller
+    {
+        static SlotMachine SlotMachine = new SlotMachine();
+
+        public IActionResult Index()
+        {
+            SlotMachine.Losuj();
+            return View("SlotMachine", SlotMachine);
+        }
+
+    }
+}
