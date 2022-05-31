f7cd3b1a8d2527c3e5636406400ffee0089b3bb9
RysiuXD <dawid.zelinka@gmail.com>
2022-05-31 15:39:23 +0200

    brakło czasu;[

--- Kasyno/Classes/SlotMachine.cs
+++ Kasyno/Classes/SlotMachine.cs
@@ -2,33 +2,64 @@
 {
     public class SlotMachine
     {
-        public int PunktyGracz, Zetony;
+        public int Zetony, BetSize, Wygrana, i=0;
         public string Komunikat;
         public bool GameOver;
         string[] DoWylosowania = { "🍒", "🍇", "💰", "🔔" };
         public string[] Wylosowane = new string[3];
+
 
         public SlotMachine()
         {
             Komunikat = "Mozesz uruchomic maszyne";
             GameOver = false;
             Zetony = 200;
-
-            
-
-            
-
-           
+            BetSize = 10;
         }
 
         public void Losuj()
         {
-            for (int i = 0; i < Wylosowane.Length; i++)
+            for (i=0; i < Wylosowane.Length; i++)
             {
                 var rnd = new Random();
                 Wylosowane[i] = DoWylosowania[rnd.Next(0, DoWylosowania.Length)];
             }
         }
+
+        public void Game_Result()
+        {
+            //if(Losuj(DoWylosowane[i]))
+            //{
+               
+            //}
+            //else
+            //{
+
+
+            //}
+
+        }
+
+        public void Rozgrywka()
+        {
+            if (Zetony >= BetSize)
+            {
+                Komunikat = "Zaczynamy";
+                GameOver = false;
+              
+                LiczZetony();
+            }
+            else
+            { Komunikat = "Masz zbyt mało żetonów"; }
+        }
+
+
+        public void LiczZetony()
+        {
+  
+        }
+
+
 
     }
 }
